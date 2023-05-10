1605e12547a20f3df45cee14f2077f0dcae7f758
Damien Davis-Neff <damiendavisneff@gmail.com>
2023-05-10 00:30:46 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.Graphics.Imaging;
+using Windows.Media.Control;
+using WindowsMediaController;
+using System.Threading;
+using System.Threading.Tasks;
+using FileLock;
+
+namespace Sample.CMD
+{
+    class Program
+    {
+        static MediaManager? mediaManager;
+        static object _writeLock = new object();
+
+        public static void Main()
+        {
+            mediaManager = new MediaManager();
+
+            mediaManager.OnAnySessionOpened += MediaManager_OnAnySessionOpened;
+            mediaManager.OnAnySessionClosed += MediaManager_OnAnySessionClosed;
+            mediaManager.OnFocusedSessionChanged += MediaManager_OnFocusedSessionChanged;
+            mediaManager.OnAnyPlaybackStateChanged += MediaManager_OnAnyPlaybackStateChanged;
+            mediaManager.OnAnyMediaPropertyChanged += MediaManager_OnAnyMediaPropertyChanged;
+
+            mediaManager.Start();
+
+            Console.ReadLine();
+            mediaManager.Dispose();
+        }
+
+        private static void MediaManager_OnAnySessionOpened(MediaManager.MediaSession session)
+        {
+            WriteLineColor("-- New Source: " + session.Id, ConsoleColor.Green);
+        }
+        private static void MediaManager_OnAnySessionClosed(MediaManager.MediaSession session)
+        {
+            WriteLineColor("-- Removed Source: " + session.Id, ConsoleColor.Red);
+        }
+
+        private static void MediaManager_OnFocusedSessionChanged(MediaManager.MediaSession mediaSession)
+        {
+            WriteLineColor("== Session Focus Changed: " + mediaSession?.ControlSession?.SourceAppUserModelId, ConsoleColor.Gray);
+        }
+
+        private static void MediaManager_OnAnyPlaybackStateChanged(MediaManager.MediaSession sender, GlobalSystemMediaTransportControlsSessionPlaybackInfo args)
+        {
+            WriteLineColor($"{sender.Id} is now {args.PlaybackStatus} - {args.PlaybackType }", ConsoleColor.Yellow);
+            WriteLineColor($"[DBG] {args}");
+        }
+
+        private static async void MediaManager_OnAnyMediaPropertyChanged(MediaManager.MediaSession sender, GlobalSystemMediaTransportControlsSessionMediaProperties args)
+        {
+            WriteLineColor($"{sender.Id} is now playing {args.Title} {(String.IsNullOrEmpty(args.Artist) ? "" : $"by {args.Artist}")}", ConsoleColor.Cyan);
+            SaveFileText($"{args.Title}","title");
+            SaveFileText($"{args.Artist}","artist");
+            while(await IsFileLocked($"{Directory.GetCurrentDirectory()}\\album.png")) {
+                await Task.Delay(300);
+            }
+            await RandomAccess_SaveImage(args.Thumbnail,"album",cts.Token);
+        }
+
+        public static void WriteLineColor(object toprint, ConsoleColor color = ConsoleColor.White)
+        {
+            lock (_writeLock)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine("[" + DateTime.Now.ToString("HH:mm:ss.fff") + "] " + toprint);
+            }
+        }
+
+        public static void SaveFileText(string towrite, string filename, bool append = false)
+        {
+            using (StreamWriter writer = new StreamWriter($"{filename}.txt",append)) {
+                    writer.WriteLine($"{towrite}");
+            }
+        }
+        
+        static CancellationTokenSource cts = new CancellationTokenSource();
+        public static async Task RandomAccess_SaveImage(IRandomAccessStreamReference streamReference, string filename, CancellationToken cancellationToken, bool overwrite = true)
+        {
+
+            while(!cancellationToken.IsCancellationRequested) {
+
+                WriteLineColor("[WARN] Skipping songs too quickly may cause the program to crash!",ConsoleColor.DarkRed);
+
+                WriteLineColor("[WARN] Image saving is a work-in-progress & is Debug Only",ConsoleColor.DarkYellow);
+
+                while(await IsFileLocked($"{Directory.GetCurrentDirectory()}\\{filename}.png")) {
+                    await Task.Delay(300);
+                }
+                if(File.Exists($"{Directory.GetCurrentDirectory()}\\{filename}.png")) File.Delete($"{Directory.GetCurrentDirectory()}\\{filename}.png");
+
+                while(await IsFileLocked($"{Directory.GetCurrentDirectory()}\\{filename}.png")) {
+                    await Task.Delay(300);
+                }
+
+                if(streamReference == null) { 
+                    await SavePlaceholder($"https://via.placeholder.com/{140}",$"{Directory.GetCurrentDirectory()}\\{filename}.png");
+                    break;
+                } else {
+                    using (var stream = await streamReference.OpenReadAsync()) {
+                        var decoder = await BitmapDecoder.CreateAsync(stream);
+                        var pixelData = await decoder.GetPixelDataAsync();
+                        var outputFile = new FileInfo($"{Directory.GetCurrentDirectory()}\\{filename}.png");
+                        using (var fileStream = outputFile.Open(FileMode.Create))
+                        {
+                            var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, fileStream.AsRandomAccessStream());
+                            encoder.SetPixelData(decoder.BitmapPixelFormat, decoder.BitmapAlphaMode, decoder.PixelWidth, decoder.PixelHeight, decoder.DpiX, decoder.DpiY, pixelData.DetachPixelData());
+                            await encoder.FlushAsync();
+                            await fileStream.FlushAsync();
+                        }
+                    }
+                }
+
+                WriteLineColor("[COMPLETE] Image Save Complete", ConsoleColor.Green);
+
+                cts.Cancel();
+                cts = new CancellationTokenSource();
+
+            }
+
+            
+            
+        }
+
+        public static async Task SavePlaceholder(string url, string filename)
+        {
+            using (var client = new HttpClient())
+            using (var stream = await client.GetStreamAsync(url))
+            using (var fileStream = new FileStream(filename, FileMode.Create))
+            await stream.CopyToAsync(fileStream);
+        }
+
+        public static async Task<bool> IsFileLocked(string filename) {
+            if(!File.Exists($"{filename}")) return false;
+            try {
+                using(FileStream stream = new FileStream(filename, FileMode.Open, FileAccess.ReadWrite, FileShare.None)) {
+                    stream.Close();
+                    await Task.Delay(100);
+                    return false;
+                }
+            } catch (IOException ex) { 
+                if(ex is FileNotFoundException || ex is DirectoryNotFoundException) return false;
+                return true; 
+            }
+        }
+
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# Now Playing for OBS
+
+This program uses [WindowsMediaController](https://github.com/DubyaDude/WindowsMediaController/) to save the title, artist, and art of the song you are currently listening to as text/images, for use in OBS.
+![image](placeholder_image)
+
+This allows you to add a now playing area to your OBS scene.
+***
+## Requirements
+- Windows 10 (Build 17763+) or Windows 11
+- .NET 4.6+ or .NET Core 3.0+ or .NET 5+
+***
+## How To Use
+1) Download The `exe` From [Releases](placeholder_link)
+2) Store It In A Safe Spot (Remember It For Later)
+3) Run The `exe` & Player Music At Least Once
+4) Open OBS Studio
+5) Add 2 Texts Source (For `artist` & `title`)
+![image](placeholder_image)
+6) Add 1 Image Source (For `album` art)
+![image](placeholder_image)
+7) Apply The `Scaling/Aspect Ratio` Filter To The Image
+![image](placeholder_image)
+8) Set The Resolution To A Resolution (NOT A RATIO) With An Aspect Ratio Of 1:1
+    - Do not select the 1:1 (or any) ratio, ensure you are choosing a resolution
+    - This ensures scaling stays consistent no matter what
+![image](placeholder_image)
+9) Open Properties For A Text Source
+10) Select `Read From File`
+![image](placeholder_image)
+11) Press Browse
+12) Locate `artist.txt` or `title.txt`
+    - These will be in the same location as the `exe` from earlier
+13) Repeat Steps 8-11 For The Other Thing
+14) Open Properties For Your Image Source
+15) Click `Browse`
+16) Locate `album.png`
+    - This will be in the location as the `exe` from earlier
+
+***
+**This is the barebones version, it does not yet have complete album art support, and documentaion is missing images
